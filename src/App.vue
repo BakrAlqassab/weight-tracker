f535d9a2e3a05cd9a338db71cfc3eb6cc0f7355a
Bakr <bakr@lamia.fi>
2023-03-17 14:06:07 +0200

    add the variables of the chart and enter values

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
-<template>
-<h1>Bonjour</h1>
-</template>
+
 
 <script>
+ /* eslint-disable */
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import chart from "chart.js/auto";
+
+const weights = ref([]);
+const weightChart = shallowRef(null);
+const weightChartEl = ref(null);
+const weightInput = ref(60.0);
+
+
+const currentWeight = computed(() => {
+  return weights.value.sort((a, b) => b.date - a.date)[0] || { weight: 0 };
+});
+
+const addWeight = () => {
+  weights.value.push({
+    weight: weightInput,
+    date: new Date.getTime(),
+  });
+};
 
 export default {
-  name: 'App',
-  components: {
-  
-  }
-}
+  name: "weight-tracker",
+
+  components: {},
+};
 </script>
 
-<style>
-</style>
+<template>
+  <main>
+    <h1>Weight Tracker</h1>
+    <div class="current">
+      <span>{{ currentWeight.weight }}</span>
+      <small>Current weight in (kg)</small>
+    </div>
+  </main>
+</template>
+
+<style></style>
